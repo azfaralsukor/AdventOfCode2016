afeb873b12f9ab41a6cdf1ad5c57078946527b5f
Azfar Alsukor <azfaralsukor@gmail.com>
2017-04-27 19:06:42 +0800

    q2=1, q1=0

--- q1.py
+++ q1.py
@@ -1,18 +1,43 @@
 from pprint import pprint
 
-with open (r"d1.txt", "r") as myfile:
-    data=myfile.read()
+with open(r"d1.txt", "r") as myfile:
+    data = myfile.read()
 
 arr = data.split(', ')
 
-pprint(arr)
+# pprint(arr)
 
-xaxis=0
-yaxis=0
+arr = ['R2', 'R2', 'R2']
+
+direction = '^'
+xaxis = 0
+yaxis = 0
 
 for cmd in arr:
-	print(cmd[1:])
-	if cmd[0] == 'L':
-		xaxis +=1
+    if cmd[0] == 'L' and direction == '^':
+        xaxis -= int(cmd[1:])
+        direction = '<'
+    if cmd[0] == 'R' and direction == '^':
+        xaxis += int(cmd[1:])
+        direction = '>'
+    if cmd[0] == 'L' and direction == 'v':
+        xaxis += int(cmd[1:])
+        direction = '>'
+    if cmd[0] == 'R' and direction == 'v':
+        xaxis -= int(cmd[1:])
+        direction = '<'
+    if cmd[0] == 'L' and direction == '>':
+        yaxis += int(cmd[1:])
+        direction = '^'
+    if cmd[0] == 'R' and direction == '>':
+        yaxis -= int(cmd[1:])
+        direction = 'v'
+    if cmd[0] == 'L' and direction == '<':
+        yaxis -= int(cmd[1:])
+        direction = 'v'
+    if cmd[0] == 'R' and direction == '<':
+        yaxis += int(cmd[1:])
+        direction = '^'
 
 print(xaxis)
+print(yaxis)
