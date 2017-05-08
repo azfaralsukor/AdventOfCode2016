abe78c6287bd4625f9efa189605591daa5c8c4d1
Azfar Alsukor <azfaralsukor@gmail.com>
2017-05-08 12:56:36 +0800

    q1 part 2 done

--- q1.py
+++ q1.py
@@ -7,11 +7,13 @@ arr = data.split(', ')
 
 # pprint(arr)
 
-# arr = ['R5', 'L5', 'R5', 'R3']
+# arr = ['R8', 'R4', 'R4', 'R8']
 
 direction = '^'
 xaxis = 0
 yaxis = 0
+x_b4 = 0
+y_b4 = 0
 counter = 1
 
 # Creates a list containing 5 lists, each of 8 items, all set to 0
@@ -19,6 +21,8 @@ w, h = 999, 999
 Matrix = [[0 for x in range(w)] for y in range(h)]
 
 for cmd in arr:
+    x_b4 = xaxis
+    y_b4 = yaxis
     if cmd[0] == 'L' and direction == '^':
         xaxis -= int(cmd[1:])
         direction = '<'
@@ -27,6 +31,14 @@ for cmd in arr:
         xaxis += int(cmd[1:])
         direction = '>'
         condition_number = 2
+        for k in range((x_b4 + 1), (xaxis + 1)):
+            if Matrix[k][yaxis] == 0:
+                Matrix[k][yaxis] = 1
+            elif Matrix[k][yaxis] == 1:
+                print("(" + str(k) + ", " + str(yaxis) + ")")
+                print(abs(k) + abs(yaxis))
+                exit()
+        print("^")
     elif cmd[0] == 'L' and direction == 'v':
         xaxis += int(cmd[1:])
         direction = '>'
@@ -35,6 +47,14 @@ for cmd in arr:
         xaxis -= int(cmd[1:])
         direction = '<'
         condition_number = 4
+        for k in range((x_b4 - 1), (xaxis - 1), -1):
+            if Matrix[k][yaxis] == 0:
+                Matrix[k][yaxis] = 1
+            elif Matrix[k][yaxis] == 1:
+                print("(" + str(k) + ", " + str(yaxis) + ")")
+                print(abs(k) + abs(yaxis))
+                exit()
+        print("v")
     elif cmd[0] == 'L' and direction == '>':
         yaxis += int(cmd[1:])
         direction = '^'
@@ -43,6 +63,14 @@ for cmd in arr:
         yaxis -= int(cmd[1:])
         direction = 'v'
         condition_number = 6
+        for k in range((y_b4 - 1), (yaxis - 1), -1):
+            if Matrix[xaxis][k] == 0:
+                Matrix[xaxis][k] = 1
+            elif Matrix[xaxis][k] == 1:
+                print("(" + str(xaxis) + ", " + str(k) + ")")
+                print(abs(xaxis) + abs(k))
+                exit()
+        print(">")
     elif cmd[0] == 'L' and direction == '<':
         yaxis -= int(cmd[1:])
         direction = 'v'
@@ -51,10 +79,16 @@ for cmd in arr:
         yaxis += int(cmd[1:])
         direction = '^'
         condition_number = 8
+        for k in range((y_b4 + 1), (yaxis + 1)):
+            if Matrix[xaxis][k] == 0:
+                Matrix[xaxis][k] = 1
+            elif Matrix[xaxis][k] == 1:
+                print("(" + str(xaxis) + ", " + str(k) + ")")
+                print(abs(xaxis) + abs(k))
+                exit()
+        print("<")
+
     print(str(counter) + ". Direction: " + direction + " Command: " + cmd[0] + cmd[1:] + " (" + str(xaxis) + ", " + str(yaxis) + ") -- " + str(condition_number))
     counter += 1
-
-# print(xaxis)
-# print(yaxis)
 
 print(abs(xaxis) + abs(yaxis))
