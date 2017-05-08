f6eaaa396be5a6b03ef2f7f991a9b1860347a06b
Azfar Alsukor <azfaralsukor@gmail.com>
2017-05-08 15:32:03 +0800

    q2 part 2 done

--- q2.py
+++ q2.py
@@ -3,11 +3,15 @@ with open(r"d2.txt", "r") as myfile:
 
 arr = data.split('\n')
 
+# arr = ['ULL', 'RRDDD', 'LURDL', 'UUUUD']
+
 current = 5
 
-# [1 2 3] [(0, 2) (1, 2), (2, 2)]
-# [4 5 6] [(0, 1) (1, 1), (2, 1)]
-# [7 8 9] [(0, 0) (1, 0), (2, 0)]
+#     1
+#   2 3 4
+# 5 6 7 8 9
+#   A B C
+#     D
 
 for input in range(0, 5):
     for x in range(0, len(arr[input])):
@@ -15,83 +19,119 @@ for input in range(0, 5):
             if arr[input][x] == 'U':
                 pass
             elif arr[input][x] == 'D':
-                current = 4
+                current = 3
             elif arr[input][x] == 'R':
-                current = 2
+                pass
             elif arr[input][x] == 'L':
                 pass
         elif current == 2:
             if arr[input][x] == 'U':
                 pass
             elif arr[input][x] == 'D':
-                current = 5
+                current = 6
             elif arr[input][x] == 'R':
                 current = 3
             elif arr[input][x] == 'L':
-                current = 1
+                pass
         elif current == 3:
-            if arr[input][x] == 'U':
-                pass
-            elif arr[input][x] == 'D':
-                current = 6
-            elif arr[input][x] == 'R':
-                pass
-            elif arr[input][x] == 'L':
-                current = 2
-        elif current == 4:
             if arr[input][x] == 'U':
                 current = 1
             elif arr[input][x] == 'D':
                 current = 7
             elif arr[input][x] == 'R':
-                current = 5
+                current = 4
             elif arr[input][x] == 'L':
-                pass
-        elif current == 5:
-            if arr[input][x] == 'U':
                 current = 2
+        elif current == 4:
+            if arr[input][x] == 'U':
+                pass
             elif arr[input][x] == 'D':
                 current = 8
             elif arr[input][x] == 'R':
+                pass
+            elif arr[input][x] == 'L':
+                current = 3
+        elif current == 5:
+            if arr[input][x] == 'U':
+                pass
+            elif arr[input][x] == 'D':
+                pass
+            elif arr[input][x] == 'R':
                 current = 6
             elif arr[input][x] == 'L':
-                current = 4
+                pass
         elif current == 6:
             if arr[input][x] == 'U':
-                current = 3
+                current = 2
             elif arr[input][x] == 'D':
-                current = 9
+                current = 'A'
             elif arr[input][x] == 'R':
-                pass
+                current = 7
             elif arr[input][x] == 'L':
                 current = 5
         elif current == 7:
             if arr[input][x] == 'U':
-                current = 4
+                current = 3
             elif arr[input][x] == 'D':
-                pass
+                current = 'B'
             elif arr[input][x] == 'R':
                 current = 8
             elif arr[input][x] == 'L':
-                pass
+                current = 6
         elif current == 8:
             if arr[input][x] == 'U':
-                current = 5
+                current = 4
             elif arr[input][x] == 'D':
-                pass
+                current = 'C'
             elif arr[input][x] == 'R':
                 current = 9
             elif arr[input][x] == 'L':
                 current = 7
         elif current == 9:
             if arr[input][x] == 'U':
-                current = 6
+                pass
             elif arr[input][x] == 'D':
                 pass
             elif arr[input][x] == 'R':
                 pass
             elif arr[input][x] == 'L':
                 current = 8
+        elif current == 'A':
+            if arr[input][x] == 'U':
+                current = 6
+            elif arr[input][x] == 'D':
+                pass
+            elif arr[input][x] == 'R':
+                current = 'B'
+            elif arr[input][x] == 'L':
+                pass
+        elif current == 'B':
+            if arr[input][x] == 'U':
+                current = 7
+            elif arr[input][x] == 'D':
+                current = 'D'
+            elif arr[input][x] == 'R':
+                current = 'C'
+            elif arr[input][x] == 'L':
+                current = 'A'
+        elif current == 'C':
+            if arr[input][x] == 'U':
+                current = 8
+            elif arr[input][x] == 'D':
+                pass
+            elif arr[input][x] == 'R':
+                pass
+            elif arr[input][x] == 'L':
+                current = 'B'
+        elif current == 'D':
+            if arr[input][x] == 'U':
+                current = 'B'
+            elif arr[input][x] == 'D':
+                pass
+            elif arr[input][x] == 'R':
+                pass
+            elif arr[input][x] == 'L':
+                pass
 
         if (x + 1) == len(arr[input]):
             print(current, end='')
